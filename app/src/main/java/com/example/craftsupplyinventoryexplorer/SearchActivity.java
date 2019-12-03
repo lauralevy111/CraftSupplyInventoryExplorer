4644138d4d1b8eb1557e805a1c9af35261f2349b
Laura Levy <llevy510@gmail.com>
2019-12-03 08:00:01 -0500

    * created SearchActivity.java and activity_search.xml

---
+++ app/src/main/java/com/example/craftsupplyinventoryexplorer/SearchActivity.java
@@ -0,0 +1,10 @@
+package com.example.craftsupplyinventoryexplorer;
+
+public class SearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
