a0aad5969e11209141549c40bc45c85b4dc4e173
Laura Levy <llevy510@gmail.com>
2019-12-12 08:18:54 -0500

    * created Pen.java extends Item  * created Pencil.java extends Item

---
+++ craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/Pen.java
@@ -0,0 +1,7 @@
+package com.example.craftsupplyinventoryexplorer2;
+
+public class Pen extends Item{
+    public Pen(String name, String inkColor, Double width){
+        super(name);
+    }
+}
---
+++ craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/Pencil.java
@@ -0,0 +1,10 @@
+package com.example.craftsupplyinventoryexplorer2;
+
+public class Pencil extends Item{
+
+    public Pencil(String name, String softness){
+        super(name);
+
+    }
+
+}
