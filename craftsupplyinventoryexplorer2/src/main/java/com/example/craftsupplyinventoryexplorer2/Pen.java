2b801149d9c4fe632929ca944bf9a4946faefca3
Laura Levy <llevy510@gmail.com>
2019-12-12 08:20:41 -0500

    * created interface Drawable.java  * implemented Drawable in Pen.java and Pencil.java

--- craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/Pen.java
+++ craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/Pen.java
@@ -1,6 +1,6 @@
 package com.example.craftsupplyinventoryexplorer2;
 
-public class Pen extends Item{
+public class Pen extends Item implements Drawable{
     public Pen(String name, String inkColor, Double width){
         super(name);
     }
--- craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/Pencil.java
+++ craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/Pencil.java
@@ -1,6 +1,6 @@
 package com.example.craftsupplyinventoryexplorer2;
 
-public class Pencil extends Item{
+public class Pencil extends Item implements Drawable{
 
     public Pencil(String name, String softness){
         super(name);
