1ba631ffa50b97756af0cfdaf07674149bf71d15
Laura Levy <llevy510@gmail.com>
2020-01-10 09:19:39 -0500

    * added balance double to DepleteAble  * added long & lat to DrawWithAble

--- craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/DepleteAble.java
+++ craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/DepleteAble.java
@@ -1,4 +1,6 @@
 package com.example.craftsupplyinventoryexplorer2;
 
 public interface DepleteAble {
+    double balance = 1;
+
 }
--- craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/DrawWithAble.java
+++ craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/DrawWithAble.java
@@ -2,4 +2,6 @@ package com.example.craftsupplyinventoryexplorer2;
 
 public interface DrawWithAble extends DepleteAble{
 
+     int latitude = 0;
+     int longitude = 0;
 }
