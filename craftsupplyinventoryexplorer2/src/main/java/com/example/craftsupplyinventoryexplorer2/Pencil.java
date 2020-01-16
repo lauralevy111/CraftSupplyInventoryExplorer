0ea8436cf79379bc017151466d0e6b194b198416
Laura Levy <llevy510@gmail.com>
2020-01-16 08:54:13 -0500

    * added lombok plugin to build gradle & used annotation @data in Pencil.java

--- craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/Pencil.java
+++ craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/Pencil.java
@@ -1,10 +1,13 @@
 package com.example.craftsupplyinventoryexplorer2;
 
+import lombok.Data;
+
+@Data
 public class Pencil extends Item implements DrawWithAble {
 
     public String softness;
 
-    public Pencil(String name, String softness){
+    public Pencil(String name, String softness) {
         super(name);
         this.softness = softness;
 
