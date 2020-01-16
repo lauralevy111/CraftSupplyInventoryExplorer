f83ae364a2d9e46d561f0315e1e0a7cb0845ee6d
Laura Levy <llevy510@gmail.com>
2020-01-16 08:48:38 -0500

    * created class PoscaMarker

---
+++ craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/PoscaMarker.java
@@ -0,0 +1,10 @@
+package com.example.craftsupplyinventoryexplorer2;
+
+public class PoscaMarker implements ColorAble {
+
+    public int colorHex;
+
+    public PoscaMarker (int colorHex){
+        this.colorHex = colorHex;
+    }
+}
