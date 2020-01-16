e28957f2ac8ac4a56a51424f865620e0ce79764b
Laura Levy <llevy510@gmail.com>
2020-01-16 08:50:08 -0500

    * added public string softness set from constructor

--- craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/Pencil.java
+++ craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/Pencil.java
@@ -2,8 +2,11 @@ package com.example.craftsupplyinventoryexplorer2;
 
 public class Pencil extends Item implements DrawWithAble {
 
+    public String softness;
+
     public Pencil(String name, String softness){
         super(name);
+        this.softness = softness;
 
     }
 
