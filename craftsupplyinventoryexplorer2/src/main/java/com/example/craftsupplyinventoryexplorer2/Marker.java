bc12d279f18ae59a0ba1ceacf8e7aedf30e59db1
Laura Levy <llevy510@gmail.com>
2020-01-17 22:08:22 -0500

    * Marker.java now saves off colorHex

--- craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/Marker.java
+++ craftsupplyinventoryexplorer2/src/main/java/com/example/craftsupplyinventoryexplorer2/Marker.java
@@ -2,7 +2,10 @@ package com.example.craftsupplyinventoryexplorer2;
 
 public class Marker extends Item implements DrawWithAble {
 
+    public int colorHex;
+
     public Marker(String name, int colorHex){
         super(name);
+        this.colorHex = colorHex;
     }
 }
